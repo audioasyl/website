a928b5d5e43b384f84c551457682354034cc6a70
Konrad Marzec <konrad.marzec@swmansion.com>
2017-03-23 10:35:43 +0100

    bg fix

--- app/components/tileDetail/TileCarousel.jsx
+++ app/components/tileDetail/TileCarousel.jsx
@@ -4,7 +4,21 @@ import { map } from 'lodash';
 import Icon from '../Icon';
 import Header from '../header/Header';
 import { tagCategoryWithTagItems } from '../../queries/tagCategory';
-
+import backgroundUrl1 from '../../../public/images/wallpapers/background1.jpg';
+import backgroundUrl2 from '../../../public/images/wallpapers/background2.JPG';
+import backgroundUrl3 from '../../../public/images/wallpapers/background3.JPG';
+import backgroundUrl4 from '../../../public/images/wallpapers/background4.JPG';
+import backgroundUrl5 from '../../../public/images/wallpapers/background5.jpeg';
+import backgroundUrl6 from '../../../public/images/wallpapers/background6.jpeg';
+import backgroundUrl7 from '../../../public/images/wallpapers/background7.jpg';
+import backgroundUrl8 from '../../../public/images/wallpapers/background8.JPG';
+import backgroundUrl9 from '../../../public/images/wallpapers/background9.JPG';
+import backgroundUrl10 from '../../../public/images/wallpapers/background10.jpg';
+import backgroundUrl11 from '../../../public/images/wallpapers/background11.jpg';
+import backgroundUrl12 from '../../../public/images/wallpapers/background12.jpg';
+import backgroundUrl13 from '../../../public/images/wallpapers/background13.jpg';
+import backgroundUrl14 from '../../../public/images/wallpapers/background14.jpg';
+import backgroundUrl15 from '../../../public/images/wallpapers/background15.jpg';
 
 import './TileCarousel.scss';
 class TileCarousel extends React.Component {
@@ -18,7 +32,10 @@ class TileCarousel extends React.Component {
   componentWillMount = () => {
     const { category, id } = this.props.router.params;
 
-    this.setState({ isLoading: true });
+    this.setState({
+      isLoading: true,
+      img: images[Math.floor(Math.random() * images.length)],
+    });
     tagCategoryWithTagItems([category], ['id'])
       .fetch()
       .on('fetch', (_, __, data) => {
@@ -49,8 +66,10 @@ class TileCarousel extends React.Component {
   }
 
   render() {
+    const style = { backgroundImage: `url('${this.state.img}')` };
+
     return (
-      <div className="TileCarousel">
+      <div className="TileCarousel" style={style}>
         <Header />
         <button
           className="TileCarousel-button TileCarousel-left"
@@ -66,6 +85,12 @@ class TileCarousel extends React.Component {
     );
   }
 }
+
+const images = [
+  backgroundUrl1, backgroundUrl2, backgroundUrl3, backgroundUrl4, backgroundUrl5,
+  backgroundUrl6, backgroundUrl7, backgroundUrl8, backgroundUrl9, backgroundUrl10,
+  backgroundUrl11, backgroundUrl12, backgroundUrl13, backgroundUrl14, backgroundUrl15,
+];
 
 TileCarousel.propTypes = {
   children: PropTypes.object.isRequired,
